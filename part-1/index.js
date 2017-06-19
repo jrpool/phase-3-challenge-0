76096b25f36e0daa17e8138dbb4994e859816de5
Jonathan Pool <pool@stulta.com>
2017-06-19 12:27:55 -0700

    Created 1st draft of part-1 solution.

---
+++ part-1/index.js
@@ -0,0 +1,64 @@
+// Load Express module.
+const express = require('express');
+
+// Use it to create an Express application.
+const app = express();
+
+// Define its response to a GET request to the URL root.
+app.get(
+  '/',
+  (request, response) => {
+    response.send(
+      'Thank you for visiting this simple calculator.'
+      + '<br><br>'
+      + 'To use it, please specify the operation you want in the address bar, '
+      + 'as shown in the following examples.'
+      + '<br><br>'
+      + 'http://127.0.0.1:8000/zero'
+      + '<br>'
+      + 'http://127.0.0.1:8000/add?a=4&b=8'
+      + '<br>'
+      + 'http://127.0.0.1:8000/subtract?a=9&b=2'
+      + '<br>'
+      + 'http://127.0.0.1:8000/double/9'
+    );
+  }
+);
+
+// Define its response to a GET request for zero.
+app.get(
+  '/zero',
+  (request, response) => {response.send('0');}
+);
+
+// Define its response to a GET request for an addition.
+app.get(
+  '/add',
+  (request, response) => {
+    response.send(request.params.a + request.params.b);
+  }
+);
+
+// Define its response to a GET request for a subtraction.
+app.get(
+  '/subtract',
+  (request, response) => {
+    response.send(request.params.a - request.params.b);
+  }
+);
+
+// Define its response to a GET request for a doubling.
+app.get(
+  '/double/:number',
+  (request, response) => {
+    response.send(2 * request.params.number);
+  }
+);
+
+// Make it listen on port 8000 and confirm to the console.
+app.listen(
+  8000,
+  () => {
+    console.log('Running on 8000.');
+  }
+);
